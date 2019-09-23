2f6dd4c8d90eae5ed2702f4aa0d78338023080a7
mateus andrade <mateus.souza@webgoal.com.br>
2019-09-23 19:16:42 -0300

    returns initial screen

--- src/game.rb
+++ src/game.rb
@@ -1,7 +1,8 @@
 class Game
   def initialize
-    @frog_x = 1
-    @frog_y = 5
+    @frog_x = 0
+    @frog_y = 4
+    @matrix
   end
 
   def up
@@ -14,12 +15,26 @@ class Game
   end
 
   def right
+    @frog_x += 1
+  end
+
+  def empty_matrix
+    [
+      [':', ':', ':', ':', ':', "\n"],
+      ['=', '=', 'O', '=', '=', "\n"],
+      ['=', '=', 'O', '=', '=', "\n"],
+      ['=', '=', 'O', '=', '=', "\n"],
+      [' ', ' ', ' ', ' ', ' ']
+    ]
   end
 
   def screen
-    ''
+    @matrix = empty_matrix
+    @matrix[@frog_y][@frog_x] = 'Q'
+    @matrix.flatten.join('')
   end
 
   def tick
+
   end
 end

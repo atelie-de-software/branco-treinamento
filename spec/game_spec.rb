87a4bf619f06f0d2a82d810d4165a63f02643e3b
Atelie <atelie@iMac-de-Vagas.local>
2019-09-23 18:55:06 -0300

    Removing frog class

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -23,6 +23,29 @@ RSpec.describe Game do
       expect(game.screen).to eq(waited_screen)
     end
   end
+  
+  describe "move to right" do
+    let(:game) { Game.new }
+
+    xit do
+      initial_screen =
+        ":::::\n" +
+        "==O==\n" +
+        "==O==\n" +
+        "==O==\n" +
+        "Q    "
+      expect(game.screen).to eq(initial_screen)
+      game.right
+
+      waited_screen =
+        ":::::\n" +
+        "==O==\n" +
+        "==O==\n" +
+        "==O==\n" +
+        " Q   "
+      expect(game.screen).to eq(waited_screen)
+    end
+  end
 
   describe "fall into water" do
     let(:game) { Game.new }

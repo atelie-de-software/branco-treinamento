aa913522e09ff72f1e07f84517736c90c80bb1a8
mateus andrade <mateus.souza@webgoal.com.br>
2019-09-23 18:52:19 -0300

    starts game class refactoring

--- src/game.rb
+++ src/game.rb
@@ -1,6 +1,11 @@
 require_relative 'world'
 
 class Game
+  def initialize
+    @frog_x = 1
+    @frog_y = 5
+  end
+
   def sobe
   end
 
@@ -14,7 +19,14 @@ class Game
   end
 
   def tela
-    ''
+    result = ''
+    result = Array.new(5) do |y|
+      Array.new(7) do |x|
+        next ' ' if y == 0 || y == (size_y - 1)
+        next 'O' if x == 4
+        next '='
+      end
+    end
   end
 
   def tick

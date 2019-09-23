74b51f6ef715ee34e3f622e484cd0245bc67b03c
Peterson Daronch de Bem <peterson@webgoal.com.br>
2019-09-23 18:54:42 -0300

    Add game screen and moves.

--- main.rb
+++ main.rb
@@ -1,17 +1,19 @@
 require 'dispel'
 require_relative 'src/game'
 
-x = 0
-y = 0
-output = ''
 game = Game.new
 # draw app and redraw after each keystroke
 Dispel::Screen.open do |screen|
   Dispel::Keyboard.output timeout: 0.5 do |key|
-    screen.draw game.output
+    screen.draw game.screen
+
     next unless key
 
     exit(true) if key == :"Ctrl+c"
-    game.update(key)
+
+    game.right if key == :right
+    game.left if key == :left
+    game.up if key == :up
+    game.down if key == :down
   end
 end

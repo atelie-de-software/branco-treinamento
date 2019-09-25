cb85002ed39479396022b3df147e143b16d8eadc
Peterson Daronch de Bem <peterson@webgoal.com.br>
2019-09-25 17:35:55 -0300

    Add sprites to game

--- main.rb
+++ main.rb
@@ -1,21 +1,37 @@
-require 'dispel'
+require 'ruby2d'
 require_relative 'src/game'
 
 game = Game.new
-# draw app and redraw after each keystroke
-Dispel::Screen.open do |screen|
-  Dispel::Keyboard.output timeout: 0.2 do |key|
-    screen.draw game.screen
 
-    next unless key
 
-    exit(true) if key == :"Ctrl+c"
+update do
+  system "clear"
 
-    game.right if key == :right
-    game.left if key == :left
-    game.up if key == :up
-    game.down if key == :down
-
-    game.tick
+  game_screen = game.screen
+  puts game_screen
+  y = 0
+  game_screen.split("\n").each do |lines|
+    x = 0
+    lines.split('').each do |sprite|
+      Image.new('water.png', x: x, y: y) if sprite == '='
+      Image.new('leaf.png', x: x, y: y) if sprite == 'O'
+      Image.new('frog.png', x: x, y: y) if sprite == 'M'
+      Image.new('start.png', x: x, y: y) if sprite == ' '
+      Image.new('finish.png', x: x, y: y) if sprite == ':'
+      Image.new('dead.png', x: x, y: y) if sprite == 'X'
+      x += 62
+    end
+    y += 62
   end
+  sleep 0.2
+  game.tick
 end
+
+on :key_down do |event|
+  game.right if event.key == 'right'
+  game.left if event.key == 'left'
+  game.up if event.key == 'up'
+  game.down if event.key == 'down'
+end
+
+show
